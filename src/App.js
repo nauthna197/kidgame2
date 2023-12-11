092e5cf324d501e45880236942c05a8e52840151
Tuan Anh <anh.nguyen5@sotatek.com>
2023-12-12 00:49:17 +0700

    upppp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import './App.css';
 import Draggable from 'react-draggable';
+import { AudioRecorder, useAudioRecorder } from "react-audio-voice-recorder";
+import { useState } from 'react';
 
 function App() {
-  const onPlayAudio = (index) => {
-    const audio = new Audio(`./audio/audio${index}.mp3`);
-    audio.play();
-  }
+  const [audioDataList, setAudioDataList] = useState([]);
+  const recorderControls = useAudioRecorder();
+  const addAudioElement = (blob) => {
+    const url = URL.createObjectURL(blob);
+    const newData = [...audioDataList];
+    newData.push(url);
+    setAudioDataList(newData);
+  };
   return (
     <div className="App">
       <div className="main">
         <div className="music-image">
+          <div className="icon-wrapper">
+            <img className="left-icon" src="./images/Picture1.png" alt="" />
+            <img className="left-icon" src="./images/Picture2.png" alt="" />
+            <img className="left-icon" src="./images/Picture3.png" alt="" />
+            <img className="left-icon" src="./images/Picture4.png" alt="" />
+          </div>
           <img src="./images/music.png" alt="" />
+          <div className="audio-wrapper">
+            <AudioRecorder
+              onRecordingComplete={(blob) => addAudioElement(blob)}
+              recorderControls={recorderControls}
+            />
+          </div>
         </div>
         <div className='card-wrapper'>
           {Array(16).fill(0).map((e, index) => {
             return <Draggable key={index} allowAnyClick={true}>
-              <div className="box" onClick={() => onPlayAudio(index + 1)}>
+              <div className="box">
                 <img src={`./images/kid${index + 1}.png`} alt="" />
               </div>
             </Draggable>
           })}
         </div>
+
       </div>
     </div>
   );
